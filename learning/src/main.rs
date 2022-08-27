ebb6ca9a77611033b95b0787ae0885ca2cd36bef
AhyaanTech <ahyaantech@gmail.com>
2022-08-27 02:44:28 -0500

    starting web development course

--- learning/src/main.rs
+++ learning/src/main.rs
@@ -1,10 +1,35 @@
+use std::fmt::Display;
+
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+struct QuestionId(String);
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+
+    fn update_title(&self, new_title: String) -> Self {
+        Question::new(self.id, new_title, self.content, self.tags)
+    }
+}
+
 fn main() {
-    let mut v = vec![1, 2, 3, 4, 5, 6];
-
-    let r = &mut v[..];
-
-    let (a, b) = r.split_at_mut(3);
-
-    assert_eq!(a, &mut [1, 2, 3]);
-    assert_eq!(b, &mut [4, 5, 6]);
+    let question = Question::new(
+        QuestionId::from("1"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        ["faq"],
+    );
+    println!("{}", question);
 }
