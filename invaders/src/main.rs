8518afb13d42320b61f81dae99a3596a2f64ea05
AhyaanTech <ahyaantech@gmail.com>
2022-09-04 02:50:09 -0500

    starting to get the player

---
+++ invaders/src/main.rs
@@ -0,0 +1,86 @@
+use std::{
+    error::Error,
+    io::{self, stdout},
+    sync::mpsc,
+    thread,
+    time::Duration,
+};
+
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use invaders::{
+    frame::{self, new_frame, Drawabale},
+    player::Player,
+    render,
+};
+use rusty_audio::Audio;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+
+    let audio_files_list = vec!["explode", "lose", "move", "pew", "startup", "win"];
+    for audio_file_name in audio_files_list {
+        audio.add(audio_file_name, format!("{}.wav", audio_file_name))
+    }
+    audio.play("startup");
+
+    // Terminal
+    let mut stdout = stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Render loop in a seperate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+
+        while let Ok(curr_frame) = render_rx.recv() {
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+    // Game Loop
+    let mut player = Player::new();
+    'gameloop: loop {
+        //Per-frame init
+        let mut curr_frame = new_frame();
+
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Right => player.move_right(),
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        // Draw and render
+        player.draw(&mut curr_frame);
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+
+    Ok(())
+}
---
+++ invaders/src/player.rs
@@ -0,0 +1,42 @@
+use crate::{
+    frame::{Drawabale, Frame},
+    NUM_COLS, NUM_ROWS,
+};
+
+pub struct Player {
+    x: usize,
+    y: usize,
+}
+
+impl Player {
+    pub fn new() -> Self {
+        Self {
+            x: NUM_COLS / 2,
+            y: NUM_ROWS - 1,
+        }
+    }
+
+    pub fn move_left(&mut self) {
+        if self.x > 0 {
+            self.x -= 1;
+        }
+    }
+
+    pub fn move_right(&mut self) {
+        if self.x < NUM_COLS - 1 {
+            self.x += 1;
+        }
+    }
+}
+
+impl Drawabale for Player {
+    fn draw(&self, frame: &mut Frame) {
+        frame[self.x][self.y] = "A";
+    }
+}
+
+impl Default for Player {
+    fn default() -> Self {
+        Self::new()
+    }
+}
